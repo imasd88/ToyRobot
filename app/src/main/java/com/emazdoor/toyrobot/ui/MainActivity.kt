c293824aff19010c843e0c610abf7d41c69d66c3
imasdurrani <demha.ahmed@gmail.com>
2020-03-14 15:12:48 +1100

    imporved code

--- app/src/main/java/com/emazdoor/toyrobot/ui/MainActivity.kt
+++ app/src/main/java/com/emazdoor/toyrobot/ui/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.emazdoor.toyrobot.ui
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.emazdoor.toyrobot.R
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.emazdoor.toyrobot.databinding.ActivityMainBinding
 import com.emazdoor.toyrobot.listener.BoardManagerListener
 import com.emazdoor.toyrobot.manager.BoardManager
 import com.emazdoor.toyrobot.models.Directions
 import com.emazdoor.toyrobot.models.GameBoard
 import com.emazdoor.toyrobot.models.Robot
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity(), BoardManagerListener {
 
-    lateinit var binding: ActivityMainBinding
-    lateinit var boardManager: BoardManager
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var boardManager: BoardManager
     private val boardSize = 5
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,10 +29,29 @@ class MainActivity : AppCompatActivity(), BoardManagerListener {
         boardManager.setBoardManagerListener(this)
         binding.apply {
             runButton.setOnClickListener {
-                boardManager.processUserCommand(command.text.toString().toUpperCase(Locale.US))
+                processCommand()
             }
+
+            commandET.setOnEditorActionListener { v, _, _ ->
+                hideKeyboard(v)
+                processCommand()
+                true
+            }
+
         }
         updateOutputView()
+    }
+
+    private fun hideKeyboard(v: View) {
+        (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).hideSoftInputFromWindow(
+            v.windowToken,
+            0
+        )
+    }
+
+    private fun processCommand() {
+        boardManager.processUserCommand(commandET.text.toString().toUpperCase(Locale.US))
+        commandET.editableText.clear()
     }
 
     override fun updatePositionDetail(x: Int, y: Int, direction: Directions) {
@@ -37,7 +59,7 @@ class MainActivity : AppCompatActivity(), BoardManagerListener {
         updateOutputView(x, y, direction)
     }
 
-    fun updateOutputView(x: Int = 0, y: Int = 0, direction: Directions = Directions.NORTH) {
+    private fun updateOutputView(x: Int = 0, y: Int = 0, direction: Directions = Directions.NORTH) {
         binding.outputTv.text = "X: $x, Y: $y, F: ${direction.name}"
     }
 }
