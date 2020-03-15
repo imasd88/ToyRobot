baf9e262f842600f575f462b8f3362cf0e7b827e
imasdurrani <demha.ahmed@gmail.com>
2020-03-15 22:07:48 +1100

    added GameBoard unit tests

--- app/src/main/java/com/emazdoor/toyrobot/models/GameBoard.kt
+++ app/src/main/java/com/emazdoor/toyrobot/models/GameBoard.kt
@@ -3,4 +3,8 @@ package com.emazdoor.toyrobot.models
 class GameBoard(private val width: Int = 5, private val height: Int = 5) {
 
     fun isValidMoveLocation(x: Int, y: Int) = ((x in 0..width) && (y in 0..height))
+
+    fun getWidth() = width
+
+    fun getHeight() = height
 }
---
+++ app/src/test/java/com/emazdoor/toyrobot/GameBoardUnitTest.kt
@@ -0,0 +1,56 @@
+package com.emazdoor.toyrobot
+
+import com.emazdoor.toyrobot.models.GameBoard
+import org.junit.Test
+
+import org.junit.Assert.*
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class GameBoardUnitTest {
+
+    private val gameBoard = GameBoard()
+
+    @Test
+    fun `test isValidMoveLocation with negative x value should return false`() {
+        assertFalse(gameBoard.isValidMoveLocation(-1, 0))
+    }
+
+    @Test
+    fun `test isValidMoveLocation with negative y value should return false`() {
+        assertFalse(gameBoard.isValidMoveLocation(0, -1))
+    }
+
+    @Test
+    fun `test isValidMoveLocation with negative x and y value should return false`() {
+        assertFalse(gameBoard.isValidMoveLocation(-1, -1))
+    }
+
+    @Test
+    fun `test isValidMoveLocation with positive x above board size should return false`() {
+        assertFalse(gameBoard.isValidMoveLocation(6, 4))
+    }
+
+    @Test
+    fun `test isValidMoveLocation with positive y value above board size should return false`() {
+        assertFalse(gameBoard.isValidMoveLocation(4, 6))
+    }
+
+    @Test
+    fun `test isValidMoveLocation with positive x and y value above board size should return false`() {
+        assertFalse(gameBoard.isValidMoveLocation(6, 6))
+    }
+
+    @Test
+    fun `test isValidMoveLocation with largest valid x and y value should return true`() {
+        assertTrue(gameBoard.isValidMoveLocation(5, 5))
+    }
+
+    @Test
+    fun `test isValidMoveLocation with smallest valid x and y value should return true`() {
+        assertTrue(gameBoard.isValidMoveLocation(0, 0))
+    }
+
+}
